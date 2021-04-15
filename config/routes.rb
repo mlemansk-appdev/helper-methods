beb06436eeccc4d1831943c7b111b60b0afbfae5
mlemansk <mlemansk@chicagobooth.edu>
2021-04-15 00:42:16 +0000

    Added directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,31 @@
 Rails.application.routes.draw do
-  # get "/", controller: "movies", action: "index" 
+  # # get "/", controller: "movies", action: "index" 
 
-  # get "/" => "movies#index"
+  # # get "/" => "movies#index"
 
-  root "movies#index"
+  
 
-  # Routes for the Movie resource:
-  #    "/path"        "controller#action"
-  # CREATE
-  post "/movies" => "movies#create" , as: :movies  #movies_url and movies_path
-  get "/movies/new" => "movies#new" , as: :new_movie
+  # # Routes for the Movie resource:
+  # #    "/path"        "controller#action"
+  # # CREATE
+  # post "/movies" => "movies#create" , as: :movies  #movies_url and movies_path
+  # get "/movies/new" => "movies#new" , as: :new_movie
           
-  # READ
-  get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" , as: :movie #movies_path and expects and argument for id segment
+  # # READ
+  # get "/movies" => "movies#index" 
+  # get "/movies/:id" => "movies#show" , as: :movie #movies_path and expects and argument for id segment
   
-  # UPDATE
-  patch "/movies/:id"  => "movies#update" 
-  get "/movies/:id/edit"  => "movies#edit" , as: :edit_movie #expects and argument for id segment
+  # # UPDATE
+  # patch "/movies/:id"  => "movies#update" 
+  # get "/movies/:id/edit"  => "movies#edit" , as: :edit_movie #expects and argument for id segment
   
-  # DELETE
-  delete "/movies/:id"  => "movies#destroy" 
+  # # DELETE
+  # delete "/movies/:id"  => "movies#destroy" 
 
-  #------------------------------
+  # #------------------------------
+  
+  root "movies#index"
+  resources :movies
+  resources :directors
+
 end
